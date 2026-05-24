4b6fe618b4fef70ab228e3c35c6631e8b7a05c81
Pixelrick420 <harikrishnanr0505@gmail.com>
2026-05-24 14:21:49 +0530

    server fix

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const PORT = 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/favicon.ico', (req, res) => res.sendFile(path.join(__dirname, 'public', 'favicon.svg')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(session({
